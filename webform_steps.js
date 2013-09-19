9195f269243e8ba20ae694c2cb6851f1e62ee81b
Alex Berger <alex@more-onion.com>
2013-09-19 13:02:42 +0200

    Use mousedown with webform_ajax.

--- webform_steps.js
+++ webform_steps.js
@@ -5,7 +5,10 @@ Drupal.behaviors.webform_steps.attach = function(context, settings) {
   $('.webform-steps-wrapper', context).click(function(event) {
     var $target = $(event.target);
     if ($target.is('span')) {
-      $target.find('input').click();
+      var $input = $target.find('input');
+      if ($input.is(':enabled')) {
+        $input.mousedown();
+      };
     }
   }).addClass('webform-steps-clickable').addClass('clickable');
 }
