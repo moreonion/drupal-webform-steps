66094b51209b49f7b07cdaccf3aaa505aaa69880
Roman Zimmermann <roman@more-onion.com>
2014-10-12 20:44:52 +0200

    First working version for webform4.
    - Makes webform's progressbar clickable (JS only)
    - The buttons are hidden.

--- webform_steps.js
+++ webform_steps.js
@@ -3,22 +3,15 @@
 Drupal.behaviors.webform_steps = {};
 Drupal.behaviors.webform_steps.attach = function(context, settings) {
 
-$('.webform-steps-wrapper', context).each(function() {
-  var $this = $(this);
-  var ajax = $this.parents('form').find('[name="webform_ajax_wrapper_id"]').length > 0;
-
-  $this.click(function(event) {
-    var $target = $(event.target);
-    if ($target.is('span')) {
-      var $input = $target.find('input');
-      if ($input.is(':enabled')) {
-        if (ajax) {
-          $input.mousedown();
-        }
-        else {
-          $input.click();
-        }
-      };
+$('.webform-client-form', context).each(function() {
+  var $form = $(this);
+  var $steps = $form.find('.webform-progressbar .webform-progressbar-page');
+  $form.find('.webform-steps-buttons input').each(function(i) {
+    var $button = $(this);
+    if ($button.is(':enabled')) {
+      $($steps[i]).click(function(event) {
+        $button.click();
+      }).addClass('clickable').css('cursor', 'pointer');
     }
   });
 });
