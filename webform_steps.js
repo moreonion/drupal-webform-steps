99b9a0e0dda7218169fdfe98e8aab8caa81f1716
Roman Zimmermann <roman@more-onion.com>
2016-04-26 10:09:09 +0200

    Issue #2713255 by Mingsong: Step buttons is not clickable in the progress bar

--- webform_steps.js
+++ webform_steps.js
@@ -6,7 +6,7 @@ Drupal.behaviors.webform_steps.attach = function(context, settings) {
 $('.webform-client-form', context).each(function() {
   var $form = $(this);
   var $steps = $form.find('.webform-progressbar .webform-progressbar-page');
-  $form.find('.webform-steps-buttons input.step-button').each(function(i) {
+  $form.find('.webform-steps-buttons .step-button').each(function(i) {
     var $button = $(this);
     if ($button.is(':enabled')) {
       $($steps[i]).click(function(event) {
